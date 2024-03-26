7f010fed6acbac2d2384d5e12c91e21e8f1ed7d6
Khushi2629 <121286189+Khushi2629@users.noreply.github.com>
2024-03-26 22:58:22 +0530

    Create leetcode_114.cpp

---
+++ leetcode_114.cpp
@@ -0,0 +1,39 @@
+Given the root of a binary tree, flatten the tree into a "linked list":
+
+The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
+The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+
+
+
+
+  class Solution {
+public:
+void helper(TreeNode* root,TreeNode* &store){
+    if(root->left==NULL && root->right==NULL){
+        store=root;
+        return;
+    }
+    if(root->left){
+        helper(root->left,store);
+    }
+    TreeNode* temp=root->right;
+    if(store!=NULL){
+        root->right=root->left;
+        store->right=temp;
+    }
+    root->left=NULL;
+    if(temp){
+        store=NULL;
+        helper(temp,store);
+    }
+}
+    void flatten(TreeNode* root) {
+        if(root==NULL || (root->left==NULL && root->right==NULL)){
+            return;
+        }
+        TreeNode* store=NULL;
+        helper(root,store);
+
+        
+    }
+};
