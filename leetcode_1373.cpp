ff22aef735450e9e98c3288c60d45e926b9fc8c4
Khushi2629 <121286189+Khushi2629@users.noreply.github.com>
2024-04-16 23:42:52 +0530

    Create leetcode_1373.cpp

---
+++ leetcode_1373.cpp
@@ -0,0 +1,51 @@
+Given a binary tree root, return the maximum sum of all keys of any sub-tree which is also a Binary Search Tree (BST).
+
+Assume a BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+
+
+
+
+
+
+
+
+
+
+class nodeValue{
+     public:
+     int min;
+     int max;
+     int sum;
+     nodeValue(int min,int max,int sum)
+     {
+         this->min=min;
+         this->max=max;
+         this->sum=sum;
+     }
+ };
+class Solution {
+public:
+    int ans=0;
+    int maxSumBST(TreeNode* root) {
+        fun(root);
+        return ans;
+    }
+    nodeValue fun(TreeNode* root)
+    {
+        if(root==NULL)
+        return nodeValue(INT_MAX,INT_MIN,0);
+        nodeValue l=fun(root->left);
+        nodeValue r=fun(root->right);
+        if(root->val>l.max && root->val<r.min)
+        {
+            ans=max(ans,root->val+l.sum+r.sum);
+            return nodeValue(min(root->val,l.min),max(root->val,r.max),l.sum+root->val+r.sum);
+        }
+        return nodeValue(INT_MIN,INT_MAX,max(l.sum,r.sum));
+    }
+
+};
