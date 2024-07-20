433332d122976356dc71baf49810d1b5d4baac3a
Khushi2629 <121286189+Khushi2629@users.noreply.github.com>
2024-07-20 19:46:50 +0530

    Create leetcode_1605.cpp

---
+++ leetcode_1605.cpp
@@ -0,0 +1,35 @@
+You are given two arrays rowSum and colSum of non-negative integers where rowSum[i] is the sum of the elements in the ith row and colSum[j] is the sum of the elements of the jth column of a 2D matrix. In other words, you do not know the elements of the matrix, but you do know the sums of each row and column.
+
+Find any matrix of non-negative integers of size rowSum.length x colSum.length that satisfies the rowSum and colSum requirements.
+
+Return a 2D array representing any matrix that fulfills the requirements. It's guaranteed that at least one matrix that fulfills the requirements exists.
+
+
+
+
+class Solution {
+public:
+    vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
+        int numRows=rowSum.size();
+        int numCols=colSum.size();
+        vector<vector<int>> result(numRows,vector<int>(numCols,0));
+
+        int i=0,j=0;
+
+        while(i<numRows && j<numCols) {
+            int val=min(rowSum[i], colSum[j]);
+            result[i][j]=val;
+            rowSum[i] -=val;
+            colSum[j]-=val;
+            if(rowSum[i]==0){
+                i++;
+            }
+            if(colSum[j]==0){
+                j++;
+            }
+        }
+        return result;
+        
+    }
+};
+
