dd9c5b3c9e7b830b0574388081f8b1225beaa56f
Khushi <121286189+Khushi2629@users.noreply.github.com>
2025-03-19 20:50:22 +0530

    Update leetcode_743.cpp

--- leetcode_743.cpp
+++ leetcode_743.cpp
@@ -7,49 +7,45 @@ We will send a signal from a given node k. Return the minimum time it takes for 
 
 
 
-
-
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<vector<pair<int, int>>> adj(n+1);  // Create adjacency list
 
-        vector<vector<pair<int,int>>> adj(n+1);
+        for (auto &e : times)  // FIXED: Removed the dot `.`
+            adj[e[0]].push_back({e[1], e[2]}); 
 
-        for(auto &e : times).    //The loop iterates over each edge in the times vector
-        adj[e[0]].push_back({e[1],e[2]});     // adds the pair (v, w) to the list of edges for node u, where e[0] represents the strting node, e[1] represent ending node, e[2] represent the time taken.
+        vector<int> dis(n+1, INT_MAX);  // Distance vector initialized with INT_MAX
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
 
-        vector<int>dis(n+1,INT_MAX);
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>q;
+        q.push({0, k});  // Start from node k
+        dis[k] = 0;
 
-        q.push({0,k});
-        dis[k]=0;
-        while(!q.empty())
-        {
-            int u=q.top().second;
-            int d=q.top().first;
+        while (!q.empty()) {
+            int u = q.top().second;
+            int d = q.top().first;
             q.pop();
 
-            if(d>dis[u]) continue;
-            for(auto & it:adj[u])
-            {
-                int v=it.first;
-                int w=it.second;
-                if(dis[v]>dis[u]+w)
-                {
-                    dis[v]=dis[u]+w;
-                    q.push({dis[v],v});
+            if (d > dis[u]) continue;  // If a better distance is already found
+
+            for (auto &it : adj[u]) {
+                int v = it.first;
+                int w = it.second;
+                if (dis[v] > dis[u] + w) {  // Relaxation step
+                    dis[v] = dis[u] + w;
+                    q.push({dis[v], v});
                 }
             }
-
         }
 
-        int mini=INT_MIN;
-        for(int i=1;i<=n;i++){
-            if(dis[i]==INT_MAX) return -1;
-            if(dis[i]>mini)mini=dis[i];
+        int mini = 0;  // FIXED: Initialize with 0, not INT_MIN
+        for (int i = 1; i <= n; i++) {
+            if (dis[i] == INT_MAX) return -1;  // If any node is unreachable
+            mini = max(mini, dis[i]);  // Track the longest shortest time
         }
 
         return mini;
-        
     }
 };
+
+
