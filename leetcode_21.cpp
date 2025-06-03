2f74942d04c8c4b4bb58d231d51cc4f985ef3dff
Khushi <121286189+Khushi2629@users.noreply.github.com>
2025-06-03 22:38:59 +0530

    Update leetcode_21.cpp

--- leetcode_21.cpp
+++ leetcode_21.cpp
@@ -7,18 +7,19 @@ Return the head of the merged linked list.
 
 
 
-
-  class Solution {
+class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(!list1) return list2;
-        if(!list2) return list1;
+        if (!list1) return list2;
+        if (!list2) return list1;
 
-        if(list1->val >list2->val){
-            swap(list1,list2);
+        if (list1->val > list2->val) {
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
+        } else {
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
         }
-        list1->next=mergeTwoLists(list1->next,list2);
-        return list1;
         
     }
 };
