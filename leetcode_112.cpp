63c8da3f854356aaddcf15cdf370d46759700211
Khushi <121286189+Khushi2629@users.noreply.github.com>
2025-06-23 12:14:41 +0530

    Create leetcode_112.cpp

---
+++ leetcode_112.cpp
@@ -0,0 +1,21 @@
+Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+A leaf is a node with no children.
+
+
+
+
+
+
+  class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(root==NULL) return false;
+        if(!root->left && !root->right){     //check if its a leaf node
+            return targetSum-root->val==0;   // if a leaf node then check if diff is 0
+        }
+        targetSum-=root->val;                // “If I’m at a node with value x, then the remaining sum I need to find in the child paths is targetSum - x.”
+
+         return hasPathSum(root->left,targetSum) || hasPathSum(root->right,targetSum);
+    }
+};
