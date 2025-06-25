9116d9b1ac53cecc819f96ed7467fd13a77d965e
Khushi <121286189+Khushi2629@users.noreply.github.com>
2025-06-25 16:25:12 +0530

    Update leetcode_119 (V.Imp).cpp

--- leetcode_119 (V.Imp).cpp
+++ leetcode_119 (V.Imp).cpp
@@ -6,17 +6,23 @@ In Pascal's triangle, each number is the sum of the two numbers directly above i
 
 
 
+
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-        vector<int>result(rowIndex+1,1);
-        for(int i=1;i<rowIndex;i++){
-            for(int j=i;j>0;j--){
-                result[j]+=result[j-1];
-            }
-        }
-        return result;
+        vector<int> row = {1}; // row 0 is always [1]
 
-        
+        for (int i = 1; i <= rowIndex; i++) {
+            vector<int> newRow(i + 1, 1); // create row of size i+1, all 1s
+
+            for (int j = 1; j < i; j++) {
+                newRow[j] = row[j - 1] + row[j]; // compute inner elements
+            }
+
+            row = newRow; // move to next row
+        }
+
+        return row;
     }
 };
+
