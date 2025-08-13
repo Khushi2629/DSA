ddefc7d41598c7b37ae4ab74e7d7186090842b7e
Khushi <121286189+Khushi2629@users.noreply.github.com>
2025-08-13 20:15:44 +0530

    Create leetcode_503.cpp

---
+++ leetcode_503.cpp
@@ -0,0 +1,32 @@
+Given a circular integer array nums (i.e., the next element of nums[nums.length - 1] is nums[0]), return the next greater number for every element in nums.
+
+The next greater number of a number x is the first greater number to its traversing-order next in the array, which means you could search circularly to find its next greater number. If it doesn't exist, return -1 for this number.
+
+
+
+
+
+  class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>ans(n,-1);
+        stack<int>st;
+        for(int i=0;i < 2*n;i++){
+            int index = i % n;
+
+            while(!st.empty() && nums[index] > nums[st.top()]){
+                ans[st.top()]= nums[index];
+                st.pop();
+            }
+            if(i<n) st.push(index);
+            
+
+
+        }
+        return ans;
+
+
+        
+    }
+};
